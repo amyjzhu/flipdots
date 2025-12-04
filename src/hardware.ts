7df695c216943abf9deeeccf0ffdc1a87b1cbb5b
Amy Zhu <ajyzhu@gmail.com>
2025-12-04 14:28:11 -0800

    keep working on implementation

--- src/hardware.ts
+++ src/hardware.ts
@@ -1,17 +1,26 @@
 interface HardwareInterface {
     units: Unit[][] // need to map these somewhere somehow
     unitAdjacency: (toCheck: UnitId) => UnitId[];
-    allowedNextActive: (action: Action, id: UnitId, time: Time) => [UnitId[], Time];
-    actionToHardwareAction: (action: Action, id: UnitId, time: Time) => void;
+    allowedNextActive: (action: Action, id: UnitId[], time: Time) => [UnitId[], Time];
+    actionsToHardwareAction: (action: Action, id: UnitId[], time: Time) => void;
 }
 
-type Time = number | [number, Action];
+type Time = number | [number, Action, number];
 type UnitId = number;
-type StateId = number; 
+type StateId = number;
 
-class Unit {
-    actions: Action[] = [];
-    states: [Action, [StateId, State][]][] = [];
+let incrementTime = (t: Time, inc: number) => {
+    if (typeof t == "number") {
+        return t + inc;
+    } else {
+        return [t[0], t[1], t[2] + inc]
+    }
+}
+
+interface Unit {
+    id: UnitId;
+    actions: Action[];
+    states: [Action, [StateId, State][]][];
 }
 
 enum Action {
@@ -21,7 +30,7 @@ enum Action {
 }
 
 class Colour {
-    rgb: [number, number, number] = [0,0,0];
+    rgb: [number, number, number] = [0, 0, 0];
 }
 
 class State {
@@ -45,8 +54,106 @@ interface Transition {
 
 class FlipdotHardware implements HardwareInterface {
     units: Unit[][];
+    unitIdToUnit: Map<UnitId, Unit>;
     unitAdjacency: (toCheck: UnitId) => UnitId[];
-    allowedNextActive: (action: Action, id: UnitId, time: Time) => [UnitId[], Time];
-    actionToHardwareAction: (action: Action, id: UnitId, time: Time) => void;
+    allowedNextActive: (action: Action, id: UnitId[], time: Time) => [UnitId[], Time];
+    actionsToHardwareAction: (action: Action, id: UnitId[], time: Time) => void;
 
+    constructor(units: Unit[][], adjacency: (toCheck: UnitId) => UnitId[]) {
+        this.units = units;
+        this.unitIdToUnit = new Map();
+        for (let u of this.units.flat()) {
+            this.unitIdToUnit.set(u.id, u);
+        }
+
+        this.unitAdjacency = adjacency;
+        this.allowedNextActive = (action: Action, ids: UnitId[], time: Time) => {
+            // is this true?
+            // surely it takes some time for units to flip!
+            return [this.units.map(r => r.map(u => u.id)).flat(), incrementTime(time, 1)] as [UnitId[], Time];
+        }
+
+
+
+        this.actionsToHardwareAction = (action: Action, ids: UnitId[], time: Time) => {
+            ids.forEach(id => console.log(`${action}, ${id}`));
+            console.log(`wait ${time}`);
+        }
+
+        let compile = (groupActions: GroupAction[]) => {
+            for (let ga of groupActions) {
+                let time = ga.tPlus;
+                let possibleTime // keep track.... 
+                let actionSet = ga.actions;
+                for (let action of actionSet) {
+                    let actionType: Action = action[0];
+                    let units: Unit[] = action[1].map(i => this.unitIdToUnit.get(i)!);
+
+                    // first, I need to check the timing by inputting the group action and seeing 
+                    // if it's possible.
+                    // first, make sure we can do everything simultaneously
+
+                
+                    let [avail, nextTime] = this.allowedNextActive(actionType, action[1], time);
+                    
+                    // if nextTime is before the next set of actions, yay 
+
+                    for (let unit of units) {
+                        if (unit.actions.includes(action[0])) {
+                            // proceed
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+    static Rectangular(width: number, height: number, backCol: Colour, frontCol: Colour) {
+        let unitList = [...new Array(height).keys()].map(i => [...new Array(width).keys()].map(j => new FlipdotUnit(backCol, frontCol, i * height + j)));
+
+        let adjacency = (i: UnitId) => {
+            let neighbours: UnitId[] = [];
+            // if we're at the edge, don't include some:
+
+            let xCoord = i % width;
+            let yCoord = Math.floor(i / width);
+
+            for (let yPlus of [-1, 0, 1]) {
+                for (let xPlus of [-1, 0, 1]) {
+                    if (!(xPlus == 0 && yPlus == 0) ||
+                        (xCoord + xPlus >= width || xCoord + xPlus < 0
+                            || yCoord + yPlus < 0 || yCoord + yPlus >= height)) {
+                        neighbours.push(i + yPlus * width + xPlus);
+                    }
+                }
+            }
+            console.log(neighbours);
+            return neighbours;
+        }
+
+        return new FlipdotHardware(unitList, adjacency);
+    }
+
+
+}
+
+class FlipdotUnit implements Unit {
+    id: UnitId;
+    actions: Action[];
+    states: [Action, [StateId, State][]][];
+
+    constructor(backCol: Colour, frontCol: Colour, id: UnitId) {
+        this.id = id;
+        this.actions = [Action.FLIP];
+        this.states = [[Action.FLIP, [[0, new FlipdotState(backCol)], [1, new FlipdotState(frontCol)]]]];
+    }
+}
+
+
+class FlipdotState implements State {
+    image: Colour[][];
+    constructor(col: Colour) {
+        this.image = [[col]];
+    }
 }
